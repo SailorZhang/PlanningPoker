248bf23537fc2d2d1975fb06567d232f2c6c67cd
xiangnan.zhang <z@zhangxiangnan.cn>
2017-10-04 19:16:53 +0800

    第二章结束

--- src/app/stock/stock-manage/stock-manage.component.ts
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -7,20 +7,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StockManageComponent implements OnInit {
 
-  private stocks: Array<Stock>;
+  public stocks: Array<Stock>;
+
   constructor() {
   }
 
   ngOnInit() {
-    this.stocks=[
-      new Stock(1,"第一只股票",1.99,3.5,"这是第一只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(2,"第二只股票",1.99,3.5,"这是第二只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(3,"第三只股票",1.99,3.5,"这是第三只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(4,"第四只股票",1.99,3.5,"这是第四只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(5,"第五只股票",1.99,3.5,"这是第五只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(6,"第六只股票",1.99,3.5,"这是第六只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(7,"第七只股票",1.99,3.5,"这是第七只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
-      new Stock(8,"第八只股票",1.99,3.5,"这是第八只股票，我在慕课网学习angular实战课程。",["IT","互联网"]),
+    this.stocks = [
+      new Stock(1, "第一只股票", 1.99, 3.5, "这是第一只股票，我在慕课网学习angular实战课程。", ["IT", "互联网"]),
+      new Stock(2, "第二只股票", 2.99, 2.5, "这是第二只股票，我在慕课网学习angular实战课程。", ["IT", "金融"]),
+      new Stock(3, "第三只股票", 3.99, 3.4, "这是第三只股票，我在慕课网学习angular实战课程。", ["互联网"]),
+      new Stock(4, "第四只股票", 4.99, 3, "这是第四只股票，我在慕课网学习angular实战课程。", ["IT"]),
+      new Stock(5, "第五只股票", 5.99, 1.5, "这是第五只股票，我在慕课网学习angular实战课程。", ["IT", "互联网","金融"]),
+      new Stock(6, "第六只股票", 6.99, 5, "这是第六只股票，我在慕课网学习angular实战课程。", ["IT"]),
+      new Stock(7, "第七只股票", 7.99, 4.6, "这是第七只股票，我在慕课网学习angular实战课程。", ["金融", "互联网"]),
+      new Stock(8, "第八只股票", 8.99, 1, "这是第八只股票，我在慕课网学习angular实战课程。", ["互联网"]),
     ];
   }
 
