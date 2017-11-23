deea81ea5b703a557907ebe1b9629f17b4928004
xiangnan.zhang <z@zhangxiangnan.cn>
2017-11-24 00:13:54 +0800

    6-11 completed

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,22 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit,OnChanges {
 
   @Input()
-    rating:number = 0;
+    rating: number = 0;
 
-  stars:Array<boolean>;
-  constructor() { }
+  @Input()
+  readonly: boolean = false;
+
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
+
+  stars: Array<boolean>;
+
+  constructor() {
+  }
 
   ngOnInit() {
-    this.stars=[];
-    for(let i=1;i<=5;i++) {
-      this.stars.push(i >this.rating);
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating);
+    }
+  }
+
+  setStar(index: number) {
+    if (!this.readonly) {
+      this.rating = index + 1;
+
+      this.ratingChange.emit(this.rating);
     }
   }
 
